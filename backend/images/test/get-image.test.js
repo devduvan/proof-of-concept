0c12f6783bf1ce05f14e1e77dcd730d526acb3b3
Duvan Felipe Silva <duvanfesiro@gmail.com>
2022-01-10 13:49:39 -0500

    adding test for get imaeg

---
+++ backend/images/test/get-image.test.js
@@ -0,0 +1,21 @@
+const supertest = require("supertest");
+
+const api = supertest(process.env.API_URL);
+
+test("Image not found", async () => {
+  const { body } = await api
+    .get(`/images/not`).expect(404);
+});
+
+test("Image retrieved with success", async () => {
+  const createdRequestResponse = await api
+    .post("/images")
+    .attach("image", `${__dirname}/images/200.jpeg`).expect(201);
+    
+  const imageCreated = createdRequestResponse.body.image;
+
+  const { body } = await api
+    .get(`/images/${imageCreated.id}`).expect(200);
+
+  expect(imageCreated).toStrictEqual(body);
+});
