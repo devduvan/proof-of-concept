214d15ea6bf42f0f152afbcad72f07165239ea79
Duvan Felipe Silva <duvanfesiro@gmail.com>
2022-01-13 03:44:15 -0500

    changes en .env for support layer-dir and adding plugin serverless-dotenv

--- backend/images/test/setup.js
+++ backend/images/test/setup.js
@@ -1,11 +1,10 @@
 const dotenv = require("dotenv");
 
 process.env.ENV = "dev";
-process.env.GENERAL_LAYER_DIR = `${__dirname}/../../layers/src/general/nodejs`;
 process.env.LAYER_DIR = "./layer/nodejs";
 process.env.API_BUCKET_NAME = "thumbnail-generator-api-images-dev";
 process.env.IMAGES_TABLE_NAME = "thumbnail-generator-api-images-dev";
 process.env.IMAGE_CREATED_TOPIC_ARN =
   "arn:aws:sns:us-east-1:000000000000:thumbnail-generator-api-image-created-dev";
 
-dotenv.config({ path: `${__dirname}/../../tests/.env` });
+dotenv.config({ path: `${__dirname}/../../.env` });
--- backend/thumbnails/test/setup.js
+++ backend/thumbnails/test/setup.js
@@ -1,11 +1,10 @@
 const dotenv = require("dotenv");
 
 process.env.ENV = "dev";
-process.env.GENERAL_LAYER_DIR = `${__dirname}/../../layers/src/general/nodejs`;
 process.env.LAYER_DIR = "./layer/nodejs";
 process.env.API_BUCKET_NAME = "thumbnail-generator-api-images-dev";
 process.env.THUMBNAILS_TABLE_NAME = "thumbnail-generator-api-thumbnails-dev";
 process.env.THUMBNAILS_GENERATED_TOPIC_ARN =
   "arn:aws:sns:us-east-1:000000000000:thumbnail-generator-api-thumbnails-generated-dev";
 
-dotenv.config({ path: `${__dirname}/../../tests/.env` });
+dotenv.config({ path: `${__dirname}/../../.env` });
